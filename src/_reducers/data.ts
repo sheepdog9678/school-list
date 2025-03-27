a551b6be3a82f6de76fdfb3b057db231eb00df37
sheepdog9678 <sheepdog13@naver.com>
2025-03-28 02:10:32 +0900

    feat : 정적 데이터 axios로 불러오기

--- src/_reducers/data.ts
+++ src/_reducers/data.ts
@@ -1,21 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { data } from "../types/data";
+import axios from "axios";
 
-const initialState: data = {
-  school: "",
-  location: "",
-  address: "",
-  Harvard: 0,
-  Princeton: 0,
-  MIT: 0,
-  Total: 0,
-  key: "public",
+interface DataState {
+  data: data[]; // 배열 형태의 데이터
+  loading: "idle" | "pending" | "succeeded"; // 비동기 작업 상태
+}
+
+const initialState: DataState = {
+  data: [],
+  loading: "idle",
 };
+
+const asynsGetFetch = createAsyncThunk("itemSlice/asynsGetFetch", async () => {
+  const resp = await axios.get("http://localhost:3001/data");
+  return resp.data;
+});
 
 export const dataSlice = createSlice({
   name: "item",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(asynsGetFetch.pending, (state, action) => {
+      state.loading = "pending";
+    });
+    builder.addCase(asynsGetFetch.fulfilled, (state, action) => {
+      state.loading = "succeeded";
+      state.data = action.payload;
+    });
+  },
 });
 
+export { asynsGetFetch };
 export default dataSlice.reducer;
--- src/components/views/HomePage.tsx
+++ src/components/views/HomePage.tsx
@@ -1,8 +1,18 @@
 import styled from "styled-components";
+import { asynsGetFetch } from "../../_reducers/data";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { useEffect } from "react";
 
 const Wapper = styled.div``;
 
 function HomePage() {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(asynsGetFetch());
+  }, []);
+  const data = useAppSelector((state) => state.data.data);
+  const isload = useAppSelector((state) => state.data.loading);
+  console.log(data);
   return <Wapper>홈페이지 view</Wapper>;
 }
 
